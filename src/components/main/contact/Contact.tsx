e6ae889fbbb222f95960b37278e4fcbed07f0d12
Muhammad Afif <hammadaf9408@gmail.com>
2020-07-10 13:59:43 +0700

    Style for input label, alert

--- src/components/main/contact/Contact.tsx
+++ src/components/main/contact/Contact.tsx
@@ -6,6 +6,11 @@ import EmailIcon from '@material-ui/icons/Email';
 import MapIcon from '@material-ui/icons/Room';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import classNames from 'classnames';
+import { LoadingContext } from 'context/loading/loadingContext';
+import { AlertContext } from 'context/alert/alertContext';
+import { AlertType } from 'context/alert/AlertType';
+import { useApi } from 'hooks/useApi';
+import { API_ROUTES } from 'utils/Constant';
 
 interface Props {}
 
@@ -16,12 +21,27 @@ interface IContact {
   icon: any;
 }
 
+interface IMailFormat {
+  name: string;
+  from: string;
+  subject: string;
+  message: string;
+}
+
 type AllProps 
   = WithStyles<typeof styles>
   & Props;
 
 const ContactComp: React.FC<AllProps> = (props) => {
   const { classes } = props;
+  const { postOnApi } = useApi();
+  const initialValues: IMailFormat = {
+    name: '',
+    from: '',
+    subject: '',
+    message: ''
+  }
+
   const [contacts, setContacs] = React.useState<IContact[]>([
     {
       type: 'Phone',
@@ -42,6 +62,13 @@ const ContactComp: React.FC<AllProps> = (props) => {
       icon: <MapIcon />
     },
   ]);
+  const [mail, setMail] = React.useState<IMailFormat>(initialValues)
+  const [error, setError] = React.useState<Boolean>(false)
+  const loadingContext = React.useContext(LoadingContext);
+  const alertContext = React.useContext(AlertContext);
+  
+  const { loading } = loadingContext; 
+  const { setAlert, AlertJSX } = alertContext;
 
   const handleTooltip = (index: number) => {
     let newContacts = [...contacts];
@@ -52,6 +79,41 @@ const ContactComp: React.FC<AllProps> = (props) => {
       resetContacts[index].visible = false;
       setContacs(resetContacts);
     }, 1500)
+  }
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+
+    setMail({
+      ...mail,
+      [name]: value
+    })
+  }
+
+  const handleSendMail = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const { name, from, subject, message } = mail;
+    if (name && from && subject && message) {
+
+      const next = (res: any) => {
+        setAlert({
+          type: AlertType.Success,
+          message: 'Your message has been sent!'
+        })
+        setMail(initialValues);
+      }
+      
+      postOnApi(API_ROUTES.SEND_MAIL, mail, false, next);
+    } else {
+      setAlert({
+        type: AlertType.Error,
+        message: 'Data is not complete!'
+      })
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000)
+    }
   }
 
   return (
@@ -70,23 +132,37 @@ const ContactComp: React.FC<AllProps> = (props) => {
                 <h4>Get In Touch</h4>
                 <form>
                   <div className={classes.formField}>
-                    <label>Enter your name*</label>
-                    <input type="text" name="name" disabled />
+                    <label 
+                      className={classNames(classes.inputLabel, error && mail.name === '' ? classes.errorColor : classes.fontColor)}
+                    >Enter your name*</label>
+                    <input type="text" name="name" disabled={loading} value={mail.name} onChange={handleInput} />
                   </div>
                   <div className={classes.formField}>
-                    <label>Enter your email*</label>
-                    <input type="text" name="email" disabled />
+                    <label 
+                      className={classNames(classes.inputLabel, error && mail.from === '' ? classes.errorColor : classes.fontColor)}
+                    >Enter your email*</label>
+                    {/* pattern only work if the submit is input not button */}
+                    <input type="email" name="from" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} value={mail.from} onChange={handleInput} />
                   </div>
                   <div className={classes.formField}>
-                    <label>Enter your subject*</label>
-                    <input type="text" name="subject" disabled />
+                    <label 
+                      className={classNames(classes.inputLabel, error && mail.subject === '' ? classes.errorColor : classes.fontColor)}
+                    >Enter your subject*</label>
+                    <input type="text" name="subject" disabled={loading} value={mail.subject} onChange={handleInput} />
                   </div>
                   <div className={classes.formField}>
-                    <label>Enter your message*</label>
-                    <textarea name="message" cols={30} rows={6} disabled />
+                    <label className={classNames(classes.inputLabel, error && mail.message === '' ? classes.errorColor : classes.fontColor)}>Enter your message*</label>
+                    <textarea name="message" cols={30} rows={6} disabled={loading} value={mail.message} onChange={handleInput} />
                   </div>
                   <div className={classes.formField}>
-                    <button className={classes.button} disabled>Send Mail <span className="feature-maintenance">This feature is under construction</span> </button>
+                    {/* <button className={classes.button}>Send Mail <span className="feature-maintenance">This feature is under construction</span> </button> */}
+                    {/* <input type="submit" value="Send Mail" className={classes.button}/> */}
+                    <div style={{display: 'flex'}}>
+                      <div style={{position: 'relative'}}>
+                        <button type="submit" className={classNames(classes.button, loading && classes.buttonActive)} onClick={handleSendMail} disabled={loading}>Send Mail</button>
+                      </div>
+                      {AlertJSX}
+                    </div>
                   </div>
                 </form>
               </div>
